e303e2b6c11515876f5a9623656bae970718fd43
MauriLoi <loiacono.mauri@gmail.com>
2024-01-08 13:18:53 +0000

    Update news-lastmod-hook.rb

--- _plugins/news-lastmod-hook.rb
+++ _plugins/news-lastmod-hook.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 #
-# Check for changed posts
+# Check for changed news
 
-Jekyll::Hooks.register :posts, :post_init do |post|
+Jekyll::Hooks.register :news, :new_init do |new|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  commit_num = `git rev-list --count HEAD "#{ new.path }"`
 
   if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ new.path }"`
+    new.data['last_modified_at'] = lastmod_date
   end
 
 end
